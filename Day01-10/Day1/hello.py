2d4e71af965162f44f14da1f9539bf49a5454f05
LisaXie2019 <50814552+LisaXie2019@users.noreply.github.com>
2019-06-11 17:32:23 +0800

    add  input name

--- Day01-10/Day1/hello.py
+++ Day01-10/Day1/hello.py
@@ -4,7 +4,11 @@
 Version: 1.0
 Author: Lisa
 Date: 2019-06-04
-
 """
 
+# add by lisa 2019-06-04
 print('Hello World!')
+
+# add by lisa 2019-06-11
+name = input('Please enter your name: ')
+print('hello',name)
