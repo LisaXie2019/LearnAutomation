4dc30106df80b38720afde4f1936cab837e9479f
LisaXie2019 <50814552+LisaXie2019@users.noreply.github.com>
2019-06-14 16:43:50 +0800

    add basic operation 01

---
+++ Day01-10/Day1/basic01.py
@@ -0,0 +1,23 @@
+"""
+基础操作
+
+Version: 1.0
+Author: Lisa
+Date: 2019-06-13
+
+"""
+# -*- coding: UTF-8 -*-
+
+input("按下 enter 键退出，其他任意键显示...\n")
+total = 100+\
+        200+\
+        300
+
+print(total)
+
+"""
+这是多行注释
+你好吗
+hello
+"""
+
